212e6c5158a8b51fcdf3c7f75d797346e25374ab
Ermelo1983 <ermelo1983@gmail.com>
2016-10-04 17:44:43 +0200

    fixed price and added float

--- yogastore.rb
+++ yogastore.rb
@@ -1,12 +1,12 @@
 shopping_cart = []
 
 products = [
-  { productnr: 1234, article: "ball", price: 8.00},
-  { productnr: 1235, article: "legging", price: 12.00},
-  { productnr: 1236, article: "shirt", price: 12.00},
-  { productnr: 1237, article: "towel", price: 4.00},
-  { productnr: 1238, article: "steps", price: 6.00},
-  { productnr: 1239, article: "mat", price: 25.00},
+  { productnr: 1234, article: "ball", price: 8.30},
+  { productnr: 1235, article: "legging", price: 12.50},
+  { productnr: 1236, article: "shirt", price: 12.45},
+  { productnr: 1237, article: "towel", price: 4.50},
+  { productnr: 1238, article: "steps", price: 6.95},
+  { productnr: 1239, article: "mat", price: 25.25},
   { productnr: 1240, article: "socks", price: 3.00},]
 
   puts "Welcome to our Yogastore, whats your name?"
@@ -14,5 +14,5 @@ products = [
   puts "Hello #{name}, here is a list of supplies we are selling"
 
   products.each do |product|
-    puts " #{product[:article]}   #{product[:prize]} #{product[:productnr]}"
+    puts " #{product[:article]}   #{product[:price].to_f} #{product[:productnr].to_i}"
   end
