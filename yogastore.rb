f3ac567af7dfba45d86f4cfa5ac7f312ed7aaf23
Ermelo1983 <ermelo1983@gmail.com>
2016-10-04 17:15:51 +0200

    shoppingcart and defined products

---
+++ yogastore.rb
@@ -0,0 +1,11 @@
+shopping_cart = []
+
+products = [
+  { productnr: 1234, article: "ball", price: 8.00},
+  { productnr: 1235, article: "legging", price: 12.00},
+  { productnr: 1236, article: "shirt", price: 12.00},
+  { productnr: 1237, article: "towel", price: 4.00},
+  { productnr: 1238, article: "steps", price: 6.00},
+  { productnr: 1239, article: "mat", price: 25.00},
+  { productnr: 1240, article: "socks", price: 3.00},
+]
