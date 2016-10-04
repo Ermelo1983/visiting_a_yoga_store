4660f2372a63fc75fedb36306e531c7358ca24af
Ermelo1983 <ermelo1983@gmail.com>
2016-10-04 17:34:18 +0200

    welcomeword and listing (busy)

--- yogastore.rb
+++ yogastore.rb
@@ -7,5 +7,12 @@ products = [
   { productnr: 1237, article: "towel", price: 4.00},
   { productnr: 1238, article: "steps", price: 6.00},
   { productnr: 1239, article: "mat", price: 25.00},
-  { productnr: 1240, article: "socks", price: 3.00},
-]
+  { productnr: 1240, article: "socks", price: 3.00},]
+
+  puts "Welcome to our Yogastore, whats your name?"
+  name = gets.chomp
+  puts "Hello #{name}, here is a list of supplies we are selling"
+
+  products.each do |product|
+    puts " #{product[:article]}   #{product[:prize]} #{product[:productnr]}"
+  end
